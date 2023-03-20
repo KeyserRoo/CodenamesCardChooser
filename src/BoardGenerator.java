ddd1cd6a32147a17967ee810207a603a8235dae8
KeyserRoo <jgessek@gmail.com>
2023-03-20 01:14:40 +0100

    ver 1.1 Added graphic of cards instead of colored blocks, Added text on indicator of starting player, Minor changes to make code a bit more clear, Changed usage of KeyListener to KeyAdapter, Fixed bugged size of cards grid when below 5x5 blocks.

--- src/BoardGenerator.java
+++ src/BoardGenerator.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class BoardGenerator {
     public enum cardType {red, blue, agent, neutral}
@@ -40,42 +41,47 @@ public class BoardGenerator {
     //takes array and transforms it into its visual representation (the hardware randomizer looks that way)
     public void show(int width, int height, int redAmount, int blueAmount, int agentAmount) {
         cardType[] board = generateBoard(width, height, redAmount, blueAmount, agentAmount);
+
         JFrame frame = new JFrame();
         // create the panel that holds the grid
         JPanel gridPanel = new JPanel(new GridLayout(height, width));
+
         for (int i = 0; i < board.length / height; i++) {
             for (int j = 0; j < board.length / width; j++) {
                 //picks the color based on the entity on the panel
-                Color color = switch (board[i * height + j]) {
-                    case red -> new Color(232, 72, 72);
-                    case blue -> new Color(0, 98, 168);
-                    case agent -> Color.black;
-                    case neutral -> Color.white;
+                ImageIcon imageIcon = switch (board[i * height + j]) {
+                    case red -> new ImageIcon(Objects.requireNonNull(getClass().getResource("/blocks/red_block.png")));
+                    case blue -> new ImageIcon(Objects.requireNonNull(getClass().getResource("/blocks/blue_block.png")));
+                    case agent -> new ImageIcon(Objects.requireNonNull(getClass().getResource("/blocks/black_block.png")));
+                    case neutral -> new ImageIcon(Objects.requireNonNull(getClass().getResource("/blocks/white_block.png")));
                 };
                 JPanel panel = new JPanel() {
+                    @Override
                     public void paintComponent(Graphics g) {
                         super.paintComponent(g);
-                        g.setColor(color);
-                        g.fillRect(0, 0, getWidth(), getHeight());
+                        g.drawImage(imageIcon.getImage(),0,0,getWidth(),getHeight(),null);
                     }
                 };
                 //ensures that it looks somewhat presentable
-                panel.setPreferredSize(new Dimension(100, 100));
+                panel.setPreferredSize(new Dimension(120, 120));
                 gridPanel.add(panel);
             }
         }
 
         // create the panel that holds the bottom panel
         //this is where the info on the player who starts is shown
+        JLabel lab = new JLabel("Starting player");
         JPanel bottomPanel = new JPanel() {
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 int a = (int) (Math.random() * 10);
-                g.setColor(a % 2 == 0 ? Color.blue : Color.red);
+                g.setColor(a % 2 == 0 ? new Color(51, 80, 171) : new Color(170, 51, 56));
                 g.fillRect(0, 0, getWidth(), getHeight());
             }
         };
-        bottomPanel.setPreferredSize(new Dimension(500, 50));
+        lab.setFont(new Font("Arial",Font.BOLD,40));
+        bottomPanel.add(lab);
+        bottomPanel.setPreferredSize(new Dimension(width*120, 60));
 
         // create a panel to hold both the grid panel and the bottom panel
         JPanel mainPanel = new JPanel();
--- src/VisibleWindow.java
+++ src/VisibleWindow.java
@@ -3,13 +3,10 @@ import java.awt.*;
 import javax.swing.*;
 
 class VisibleWindow extends JFrame {
+
+    JTextField[] jTextFields = new JTextField[5];
     Container c = this.getContentPane();
     JButton computeButton;
-    JTextField widthtf = new JTextField();
-    JTextField heighttf = new JTextField();
-    JTextField redAmounttf = new JTextField();
-    JTextField blueAmounttf = new JTextField();
-    JTextField agentAmounttf = new JTextField();
 
     public VisibleWindow() {
         setupWindow();
@@ -36,124 +33,98 @@ class VisibleWindow extends JFrame {
     }
 
     private void setFields() {
-        Dimension nd = new Dimension(80, 20);
+
+        //creation of JLabels
+        String[] strings = {"Enter width (recommended 5, max 9)",
+                "Enter height (recommended 5, max 9)",
+                "Enter number of red cards (recommended 8)",
+                "Enter number of blue cards (recommended 8)",
+                "Enter number of agents (recommended 1)"
+        };
+
+        JLabel[] jLabels = new JLabel[5];
+        JPanel[] jPanels = new JPanel[5];
+
+        fieldsCreatorLabel(strings, jLabels, jPanels);
 
 
-        JLabel width = new JLabel("Enter width (recommended 5, max 9)", SwingConstants.CENTER);
-        JPanel widthpan = new JPanel();
-        widthpan.add(width);
-        widthpan.setBounds(50, 100, 300, 30);
-        c.add(widthpan);
+        //creation of JTextFields
+        JPanel[] jPanels1 = new JPanel[5];
 
-        JLabel height = new JLabel("Enter height (recommended 5, max 9)", SwingConstants.CENTER);
-        JPanel heightpan = new JPanel();
-        heightpan.add(height);
-        heightpan.setBounds(50, 150, 300, 30);
-        c.add(heightpan);
+        fieldsCreatorTextField(jTextFields, jPanels1);
 
-        JLabel redAmount = new JLabel("Enter number of red cards (recommended 8)", SwingConstants.CENTER);
-        JPanel redAmountpan = new JPanel();
-        redAmountpan.add(redAmount);
-        redAmountpan.setBounds(50, 200, 300, 30);
-        c.add(redAmountpan);
-
-        JLabel blueAmount = new JLabel("Enter number of blue cards (recommended 8)", SwingConstants.CENTER);
-        JPanel blueAmountpan = new JPanel();
-        blueAmountpan.add(blueAmount);
-        blueAmountpan.setBounds(50, 250, 300, 30);
-        c.add(blueAmountpan);
-
-        JLabel agentAmount = new JLabel("Enter number of agents (recommended 1)", SwingConstants.CENTER);
-        JPanel agentAmountpan = new JPanel();
-        agentAmountpan.add(agentAmount);
-        agentAmountpan.setBounds(50, 300, 300, 30);
-        c.add(agentAmountpan);
-
-        widthtf.setPreferredSize(nd);
-        JPanel widthtfpan = new JPanel();
-        widthtfpan.add(widthtf);
-        widthtfpan.setBounds(400, 100, 100, 30);
-        c.add(widthtfpan);
-
-        heighttf.setPreferredSize(nd);
-        JPanel heighttfpan = new JPanel();
-        heighttfpan.add(heighttf);
-        heighttfpan.setBounds(400, 150, 100, 30);
-        c.add(heighttfpan);
-
-        redAmounttf.setPreferredSize(nd);
-        JPanel redAmounttfpan = new JPanel();
-        redAmounttfpan.add(redAmounttf);
-        redAmounttfpan.setBounds(400, 200, 100, 30);
-        c.add(redAmounttfpan);
-
-        blueAmounttf.setPreferredSize(nd);
-        JPanel blueAmounttfpan = new JPanel();
-        blueAmounttfpan.add(blueAmounttf);
-        blueAmounttfpan.setBounds(400, 250, 100, 30);
-        c.add(blueAmounttfpan);
-
-        agentAmounttf.setPreferredSize(nd);
-        JPanel agentAmounttfpan = new JPanel();
-        agentAmounttfpan.add(agentAmounttf);
-        agentAmounttfpan.setBounds(400, 300, 100, 30);
-        c.add(agentAmounttfpan);
 
         this.setVisible(false);
         this.setVisible(true);
     }
 
+    private void fieldsCreatorLabel(String[] s, JLabel[] jl, JPanel[] jp) {
+        for (int i = 0; i < 5; i++) {
+            jl[i] = new JLabel(s[i], SwingConstants.CENTER);
+            jp[i] = new JPanel();
+            jp[i].add(jl[i]);
+            jp[i].setBounds(50, 100 + i * 50, 300, 30);
+            c.add(jp[i]);
+        }
+    }
+
+    private void fieldsCreatorTextField(JTextField[] jtf, JPanel[] jp) {
+        Dimension nd = new Dimension(80, 20);
+        for (int i = 0; i < 5; i++) {
+            jtf[i] = new JTextField();
+            jtf[i].setPreferredSize(nd);
+            jp[i] = new JPanel();
+            jp[i].add(jtf[i]);
+            jp[i].setBounds(400, 100 + i * 50, 100, 30);
+            c.add(jp[i]);
+        }
+    }
+
     private void logic() throws WrongInputException {
 
         //ensures that text input in text fields is indeed numbers
-        KeyListener kl = new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                char c = e.getKeyChar();
-                if (!(Character.isDigit(c) || c == KeyEvent.VK_BACK_SPACE || c == KeyEvent.VK_DELETE)) {
-                    e.consume();
+        for (JTextField jTextField : jTextFields) {
+            jTextField.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    char c = e.getKeyChar();
+                    if (!(Character.isDigit(c) || c == KeyEvent.VK_BACK_SPACE || c == KeyEvent.VK_DELETE)) {
+                        e.consume();
+                    }
                 }
-            }
-            @Override
-            public void keyPressed(KeyEvent e) {}
-            @Override
-            public void keyReleased(KeyEvent e) {}
-        };
-        widthtf.addKeyListener(kl);
-        heighttf.addKeyListener(kl);
-        redAmounttf.addKeyListener(kl);
-        blueAmounttf.addKeyListener(kl);
-        agentAmounttf.addKeyListener(kl);
+            });
+        }
 
-        //let's compute button do the work
+        //makes compute button do the work
         ActionListener listenerCompute = (event) -> {
 
             //text fields inputs going as parameters for generator
-            int w = Integer.parseInt(widthtf.getText());
-            int h = Integer.parseInt(heighttf.getText());
-            int r = Integer.parseInt(redAmounttf.getText());
-            int b = Integer.parseInt(blueAmounttf.getText());
-            int a = Integer.parseInt(agentAmounttf.getText());
+            // params are as following: [0] - width, [1] - height, [2] - red cards, [3] - blue cards, [4] - agents
+            int[] params = new int[5];
+            for (int i = 0; i < params.length; i++) {
+                String text = jTextFields[i].getText();
+                if (!(text.isEmpty())) {
+                    params[i] = Integer.parseInt(text);
+                }else{
+                    jTextFields[i].setText("0");
+                }
+            }
 
             //handles the case of making only one team
-            if(r*b==0){
-                throw new WrongInputException("Are you sure you want to play 2 team game with only one team?");
+            if (params[2] * params[3] == 0) {
+                throw new WrongInputException("Please input at least 1 card for each team");
             }
-            //handles the case of drawing the board with one of sides equal to 0
-            if(w*h==0){
-                throw new WrongInputException("You can't have a board with one of dimensions equal to 0!");
-            }
-            //handles the case of making unreasonably big board
-            if(w>9||h>9){
-                throw new WrongInputException("Please choose smaller board");
+            //handles the case of drawing the board with at least one of sides equal to 0, or more than 9
+            if ((params[0] * params[1] == 0) || (params[0] > 9 || params[1] > 9)) {
+                throw new WrongInputException("Each board dimension need to be at least 1 tile, and at most 9 tiles");
             }
             //handles the case of not fitting all cards on the board
-            if (r + b + a > w * h) {
+            if (params[2] + params[3] + params[4] > params[0] * params[1]) {
                 throw new WrongInputException("Too many cards and agents on field of that size!");
             }
 
             BoardGenerator bg = new BoardGenerator();
-            bg.show(w, h, r, b, a);
+            bg.show(params[0], params[1], params[2], params[3], params[4]);
         };
         computeButton.addActionListener(listenerCompute);
     }
